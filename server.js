cd066679a0348289d3ef011f7cf0558d9e91ba56
Emir0Bekrija <126586314+Emir0Bekrija@users.noreply.github.com>
2026-05-06 14:23:37 +0200

    Changed server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-import { createServer } from "http";
-import next from "next";
+const { createServer } = require("http");
+const next = require("next");
+const { parse } = require("url");
 
 const port = parseInt(process.env.PORT || "3000", 10);
-const app = next({ dev: false });
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(port, () => {
-    console.log(`> Ready on port ${port}`);
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
